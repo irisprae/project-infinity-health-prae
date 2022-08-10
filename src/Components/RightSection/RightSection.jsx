de6d05a78ba2990815c860c69004ec1b7497f29b
Prachak Tocheewee <prachak.toc@gmail.com>
2022-08-10 22:33:10 +0700

    edit exerciselist & pagination

---
+++ src/Components/RightSection/RightSection.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './RightSection.css'
+import NoPic from './NoPic2.png'
+
+
+const RightSection = () => {
+    let height = 100;
+    let weight = 40;
+    let HeightUnit = "cm";
+    let WeightUnit = "kg";
+    const bmi = weight/((height/100)*(height/100));
+
+    let name="NameGOHere"; 
+    let email="@MailGoHere";
+
+    return (
+      <div>
+
+        <div className='RightSection'>
+          <div className='ProfileSection'>
+            <div className='ProfileHeader'>
+              <div><div className='TopicIcon'></div></div> 
+              <div className='ProfileBar'><div className='Topic'>Profile</div>
+              <button className='editButton'>edit</button></div>
+            </div>
+            <div className='ProfilePic'>
+              <div className='InnerBolderPic' >
+                {/* <img src='/images/image1.jpg' /> */}
+                <img src={NoPic} /> 
+              </div>
+            </div>
+            <div className='NameTag'> <div className='UserName'>{name}</div> <div className='MailName Grey'>{email}</div> </div>
+            <div className='UserInfo'>
+              <div className='height'><div className='Grey'>Height</div> <div>{height} {HeightUnit}</div>  </div>
+              <div className='weight'><div className='Grey'>Weight</div> <div>{weight} {WeightUnit}</div>  </div>
+              <div className='bmi'><div className='Grey'>BMI</div> <div>{bmi}</div>  </div>
+            </div>
+          </div>
+          <div className='CalendarSection'>
+            <div className='CalendarHeader'><div className='TopicIcon'></div><div className='Topic'>Calendar</div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+export default RightSection
