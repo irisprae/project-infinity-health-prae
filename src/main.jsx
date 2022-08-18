ba344af0438b4c1eff29059d609f28877656a92f
supel2nova <chal2aznable@gmail.com>
2022-08-18 14:34:00 +0700

    auth over and addac

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,42 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import App from "./Components/App/App";
+// import Overview from "./Pages/Overview/Overview";
+// import Addactivities from "./Pages/AddActivities/Addactivities";
+// import Home from "./Pages/Home/Home";
+// import Login from "./Pages/Login/Login";
+// import Signup from "./Pages/SignUp.jsx/SignUp";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const user = localStorage.getItem("token");
+
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       {user && <Route path="/" exact element={<Overview />} />}
+//       <Route path="/" element={<App />} />
+//       <Route path="overview" element={<Overview />} />
+//       <Route path="addactivities" element={<Addactivities />} />
+//       <Route path="Home" element={<Home />} />
+//       <Route path="Login" element={<Login />} />
+//       <Route path="SignUp" element={<Signup />} />
+//       <Route path="/" element={<Navigate replace to="/login" />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./Components/App/App";
-import Overview from "./Pages/Overview/Overview";
-import Addactivities from "./Pages/AddActivities/Addactivities";
-import Home from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login";
-import Signup from "./Pages/SignUp.jsx/SignUp";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="overview" element={<Overview />} />
-      <Route path="addactivities" element={<Addactivities />} />
-      <Route path="Home" element={<Home />} />
-      <Route path="Login" element={<Login />} />
-      <Route path="SignUp" element={<Signup />} />
-    </Routes>
-  </BrowserRouter>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
 );
