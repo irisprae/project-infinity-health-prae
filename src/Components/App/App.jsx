30d464c2a9fbf5f47f9e14ee98151a399d6c3a76
supel2nova <chal2aznable@gmail.com>
2022-08-19 18:02:56 +0700

    test

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -5,21 +5,24 @@ import Login from "../../Pages/Login/Login";
 import Home from "../../Pages/Home/Home";
 import Main from "../../Pages/LogOut.jsx/LogOut";
 import Addactivities from "../../Pages/AddActivities/Addactivities";
-function App() {
-	const user = localStorage.getItem("token");
 
-	return (
-		<Routes>
-			{user && <Route path="/overview" exact element={<Overview />} />}
-			{user && <Route path="/addactivities" exact element={<Addactivities />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
+function App() {
+  const user = localStorage.getItem("token");
+
+  return (
+    <Routes>
+      {user && <Route path="/overview" exact element={<Overview />} />}
+      {user && (
+        <Route path="/addactivities" exact element={<Addactivities />} />
+      )}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
       <Route path="/logout" exact element={<Main />} />
-			<Route path="/" exact element={<Home />} />
-			<Route path="/overview" element={<Navigate replace to="/login" />} />
-			<Route path="/addactivities" element={<Navigate replace to="/login" />} />
-		</Routes>
-	);
+      <Route path="/" exact element={<Home />} />
+      <Route path="/overview" element={<Navigate replace to="/login" />} />
+      <Route path="/addactivities" element={<Navigate replace to="/login" />} />
+    </Routes>
+  );
 }
 
 export default App;
