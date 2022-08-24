67315e9c0d4d5a5102decdb839937eabf07e0d53
supel2nova <chal2aznable@gmail.com>
2022-08-24 18:26:44 +0700

    edit form regis

--- src/Pages/SignUp.jsx/SignUp.jsx
+++ src/Pages/SignUp.jsx/SignUp.jsx
@@ -5,43 +5,42 @@ import { Link, useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
 
 const Signup = () => {
-
   const [data, setData] = useState({
     firstName: "",
     lastName: "",
-    birthdate:"",
+    birthdate: "",
     gender: "",
     height: "",
     weight: "",
-    username: "",
+    email: "",
     password: "",
   });
 
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-	const handleChange = ({ currentTarget: input }) => {
-		setData({ ...data, [input.name]: input.value });
-	};
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const url = "http://localhost:8080/api/users";
-			const { data: res } = await axios.post(url, data);
-			navigate("/login");
-			console.log(res.message);
-		} catch (error) {
-			if (
-				error.response &&
-				error.response.status >= 400 &&
-				error.response.status <= 500
-			) {
-				setError(error.response.data.message);
-			}
-		}
-	};
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:8080/api/users";
+      const { data: res } = await axios.post(url, data);
+      navigate("/login");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
   return (
     <div>
       <PreLogin />
@@ -51,89 +50,88 @@ const Signup = () => {
             <form className={styles.form_container} onSubmit={handleSubmit}>
               <h4>Please provide your information</h4>
               <div>
-              <input
-                type="text"
-                placeholder="First Name"
-                name="firstName"
-                onChange={handleChange}
-                value={data.firstName}
-                required
-                className={styles.input}
-              />
-              <br></br>
-              <input
-                type="text"
-                placeholder="Last Name"
-                name="lastName"
-                onChange={handleChange}
-                value={data.lastName}
-                required
-                className={styles.input}
-              />
-              <br></br>
-              <input
-                type="text"
-                placeholder="Date of Birth: yyyy-mm-dd"
-                name="birthdate"
-                onChange={handleChange}
-                value={data.birthdate}
-                required
-                className={styles.input}
-              />
-              <br></br>
-              <input
-                type="text"
-                placeholder="Gender"
-                name="gender"
-                onChange={handleChange}
-                value={data.gender}
-                required
-                className={styles.input}
-              />
-              <br></br>
-              <input
-                type="height"
-                placeholder="Height: cms."
-                name="height"
-                onChange={handleChange}
-                value={data.height}
-                required
-                className={styles.input}
-              />
-              <br></br>
-              <input
-                type="weight"
-                placeholder="Weight: kgs."
-                name="weight"
-                onChange={handleChange}
-                value={data.weight}
-                required
-                className={styles.input}
-              />
-              </div>
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  name="firstName"
+                  onChange={handleChange}
+                  value={data.firstName}
+                  required
+                  className={styles.input}
+                />
+                <br></br>
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  name="lastName"
+                  onChange={handleChange}
+                  value={data.lastName}
+                  required
+                  className={styles.input}
+                />
+                <br></br>
+                <input
+                  type="date"
+                  placeholder="Birthdate : mm/dd/yyyy"
+                  name="birthdate"
+                  onChange={handleChange}
+                  value={data.birthdate}
+                  required
+                  className={styles.input}
+                />
+                <br></br>
+                <select
+                  className={styles.input}
+                  name="gender"
+                  value={data.gender}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value=" "> </option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
 
-              <div className="UserPass">
-              <input
-                type="username"
-                placeholder="Username"
-                name="username"
-                onChange={handleChange}
-                value={data.username}
-                required
-                className={styles.input}
-              />
-              <p>Username can only contain alphabets and numbers.</p>
-              <p>Username must not exceed 12 characters.</p>
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                onChange={handleChange}
-                value={data.password}
-                required
-                className={styles.input}
-              />
-              <p>Password requires at least 8 characters.</p>
+                <br></br>
+                <input
+                  type="height"
+                  placeholder="Height : cms."
+                  name="height"
+                  onChange={handleChange}
+                  value={data.height}
+                  required
+                  className={styles.input}
+                />
+                <br></br>
+                <input
+                  type="weight"
+                  placeholder="Weight : kgs."
+                  name="weight"
+                  onChange={handleChange}
+                  value={data.weight}
+                  required
+                  className={styles.input}
+                />
+                <br></br>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  onChange={handleChange}
+                  value={data.email}
+                  required
+                  className={styles.input}
+                />
+                <br></br>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={handleChange}
+                  value={data.password}
+                  required
+                  className={styles.input}
+                />
               </div>
 
               {error && <div className={styles.error_msg}>{error}</div>}
