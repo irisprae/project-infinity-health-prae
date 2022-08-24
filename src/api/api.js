a75bb78a24aec6b8cb6d1ca1f9c110e427616cbe
Iris Khongsook <irisprae@Iriss-MacBook-Pro.local>
2022-08-24 23:09:58 +0700

    LastLastPush

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const host = "https://infinity-health.vercel.app/";
+export const host = "https://backend-infinity.vercel.app/";
 
 export const api = axios.create({
   baseURL: `${host}`,
